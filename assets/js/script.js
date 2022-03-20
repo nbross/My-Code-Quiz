08db0224b3a00154f2789557d654ceaa6f56c872
nbross <nbross2001@gmail.com>
2022-03-20 19:17:24 -0400

    quiz now starts when the start quiz button is clicked. a timer of 50 seconds also start when the start quiz is pressed and questions and answers are generated on page

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,6 +32,8 @@ var startCard = document.querySelector("#start");
 var questionCard = document.querySelector("#questions");
 var scoreCard = document.querySelector("#score");
 var leaderboardCard = document.querySelector("#leaderboard");
+var result = document.querySelector("#result")
+var resultText = document.querySelector("result-text")
 
 // function for cards to hide
 function hideCards() {
@@ -41,3 +43,63 @@ function hideCards() {
     leaderboardCard.setAttribute("hidden", true);
 }
 
+// hides the result
+function hideResultText() {
+    result.getElementsByClassName.display = "none"
+}
+
+// global variables
+var intervalID;
+var time;
+var currentQuestion;
+
+document.querySelector("#start-button").addEventListener("click", startQuiz);
+
+function startQuiz() {
+    //hides visible cards, and shows the questions
+    hideCards();
+    questionCard.removeAttribute("hidden");
+
+    //assign 0 to currentQuestion when start button is clicked, then display the current question on the page
+    currentQuestion = 0;
+    displayQuestion();
+
+    //set total time depending on number of questions
+    time = questions.length * 10;
+
+    //executes function "countdown" every 1000ms to update time and display on page
+    intervalID = setInterval(countdown, 1000);
+
+    //invoke displayTime here to ensure time appears on the page as soon as the start button is clicked, not after 1 second
+    displayTime();
+}
+
+// reduce time by 1sec and display a new value/time, if you run out of time the quiz ends
+function countdown() {
+    time--;
+    displayTime();
+    if (time < 1) {
+        endQuiz();
+    }
+}
+
+//display time on page
+var timeDisplay = document.querySelector("#time");
+function displayTime() {
+    timeDisplay.textContent = time;
+}
+
+//display the question and answer options for the current question
+function displayQuestion() {
+    let question = questions[currentQuestion];
+    let answers = question.answers;
+
+    let h2QuestionElement = document.querySelector("#question-text");
+    h2QuestionElement.textContent = question.questionText;
+
+    for (let i = 0; i < answers.length; i++) {
+        let answer = answers[i];
+        let answerButton = document.querySelector("#answer" + i);
+        answerButton.textContent = answer;
+    }
+}
